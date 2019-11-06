0dd925942b98ecf9d2ad12b7206df505021cd90e
kly001 <karenly1961@gmail.com>
2019-11-05 21:04:58 -0500

    Custom logger middleware

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 const express = require('express'); // importing a CommonJS module
+const helmet = require("helmet")
 
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
-server.use(express.json());
+function dateLogger (req, res, next) {
+  console.log(new Date().toISOString())
+next()
+}
+
+function logger(req, res, next) {
+  console.log(`[DATE: ${new Date().toISOString()}] METHOD & URL: ${req.method} to ${req.url}`)
+  next()
+}
+
+//global middleware: runs on every request
+server.use(helmet()) //third-party middleware
+server.use(express.json()); //built-in middleware
+server.use(logger)
+
+
 
 server.use('/api/hubs', hubsRouter);
 
